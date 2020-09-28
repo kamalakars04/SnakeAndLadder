22a0e3f21aa197491ed5f7da0db80dd684bb34eb
kamalakars04 <kamalakars04@gmail.com>
2020-09-28 23:52:02 +0530

    [Kamalakar].[Refactor].Edited Efficient Code

--- Program.cs
+++ Program.cs
@@ -9,77 +9,55 @@ namespace SnakeAndLadder
             Console.WriteLine("welcome to snake and ladder");
             int NUMBER_OF_PLAYERS = 2;
             int START_POSITION = 0;
-            const int IS_PLAYER_ONE = 1;
-            const int IS_PLAYER_TWO = 2;
-            int positionOfPlayer1 = 0;
-            int positionOfPlayer2 = 0;
-            int playerNmuber = 1;
+            int presentPlayer = 1;
             int numberOnDie = 0;
             int optionNumber = 0;
             const int NO_PLAY = 0;
             const int LADDER = 1;
             const int SNAKE = 2;
             int presentChanceNumber = 0;
+            int[] playerPosition = new int[NUMBER_OF_PLAYERS];
+            int[] playerNumber = new int[NUMBER_OF_PLAYERS];
+            for (int assign = 0; assign < NUMBER_OF_PLAYERS; assign++)
+            {
+                playerNumber[assign] = assign + 1;
+                playerPosition[assign] = START_POSITION;
+            }
 
             Random random = new Random();
-           
-            while (positionOfPlayer1 < 100 && positionOfPlayer2 < 100)
+
+            while (playerPosition[presentPlayer-1] < 100)
             {
-                presentChanceNumber++;
-                switch (playerNmuber)
+                if (playerNumber[presentPlayer-1] == 1)
+                    presentChanceNumber++;
+
+                numberOnDie = random.Next(1, 7);
+                optionNumber = random.Next(0, 3);
+
+                switch (optionNumber)
                 {
-                    case IS_PLAYER_ONE:
-                    numberOnDie = random.Next(1, 7);
-                    optionNumber = random.Next(0, 3);
-
-                    switch (optionNumber)
-                    {
-                        case NO_PLAY:
-                            break;
-                        case LADDER:
-                            if (positionOfPlayer1 + numberOnDie <= 100)
-                                positionOfPlayer1 += numberOnDie;
-                            break;
-                        case SNAKE:
-                            positionOfPlayer1 -= numberOnDie;
-                            if (positionOfPlayer1 < START_POSITION)
-                                positionOfPlayer1 = 0;
-                            break;
-
-                    }
-                        if (positionOfPlayer1 != 100)
-                            playerNmuber = 2;
-                        Console.WriteLine("The position of player 1 after die roll number {0} is {1}", presentChanceNumber, positionOfPlayer1);
+                    case NO_PLAY:
                         break;
-                   
-                    case IS_PLAYER_TWO:
-
-                        numberOnDie = random.Next(1, 7);
-                        optionNumber = random.Next(0, 3);
-
-                        switch (optionNumber)
-                        {
-                            case NO_PLAY:
-                                break;
-                            case LADDER:
-                                if (positionOfPlayer2 + numberOnDie <= 100)
-                                    positionOfPlayer2 += numberOnDie;
-                                break;
-                            case SNAKE:
-                                positionOfPlayer2 -= numberOnDie;
-                                if (positionOfPlayer2 < START_POSITION)
-                                    positionOfPlayer2 = 0;
-                                break;
-
-                        }
-                        if (positionOfPlayer2 != 100)
-                            playerNmuber = 1;
-                        Console.WriteLine("The position of player 2 after die roll number {0} is {1}", presentChanceNumber, positionOfPlayer2);
+                    case LADDER:
+                        if (playerPosition[presentPlayer-1] + numberOnDie <= 100)
+                            playerPosition[presentPlayer-1] += numberOnDie;
                         break;
+                    case SNAKE:
+                        playerPosition[presentPlayer-1] -= numberOnDie;
+                        if (playerPosition[presentPlayer-1] < START_POSITION)
+                            playerPosition[presentPlayer-1] = 0;
+                        break;
+
                 }
-               
+                if (playerPosition[presentPlayer - 1] != 100)
+                    presentPlayer++;
+                if (presentPlayer > NUMBER_OF_PLAYERS)
+                    presentPlayer = 1;
+
+                Console.WriteLine("The position of player {0} after die roll number {1} is {2}",presentPlayer, presentChanceNumber, playerPosition[presentPlayer-1]);
+
             }
-            Console.WriteLine("The Winner is player number {0} and total no of turns is {1}", playerNmuber, presentChanceNumber);
+            Console.WriteLine("The Winner is player number {0} and total no of turns is {1}", presentPlayer, presentChanceNumber);
 
         }
     }
