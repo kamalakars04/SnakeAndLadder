b92b96d7eeb8ee465e9571f7d2f244ef6f1a707b
kamalakars04 <kamalakars04@gmail.com>
2020-09-28 21:39:18 +0530

    [Kamalakar].Added Display of Number Of Chances and Position

--- Program.cs
+++ Program.cs
@@ -13,9 +13,12 @@ namespace SnakeAndLadder
             const int NO_PLAY = 0;
             const int LADDER = 1;
             const int SNAKE = 2;
+            int totalNumberOfChances = 0;
+            int presentChanceNumber = 0;
             Random random = new Random();
             while (presentPosition < 100)
             {
+                presentChanceNumber++;
                 int numberOnDie = random.Next(1, 7);
                 int optionNumber = random.Next(0, 3);
                 switch (optionNumber)
@@ -33,8 +36,10 @@ namespace SnakeAndLadder
                         break;
 
                 }
+                Console.WriteLine("The position after die roll number {0} is {1}", presentChanceNumber, presentPosition);
+                totalNumberOfChances += 1;
             }
-            Console.WriteLine(presentPosition);
+            Console.WriteLine("Total number of chances taken to reach 100 is {0}", totalNumberOfChances);
 
         }
     }
