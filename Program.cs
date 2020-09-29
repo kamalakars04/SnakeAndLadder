beedd03d5b490760e9a22b39da9d3cd81c9b0cb5
kamalakars04 <kamalakars04@gmail.com>
2020-09-29 07:54:30 +0530

    [Kamalakar].[Refactor].Added some minor changes

--- Program.cs
+++ Program.cs
@@ -15,21 +15,20 @@ namespace SnakeAndLadder
             const int NO_PLAY = 0;
             const int LADDER = 1;
             const int SNAKE = 2;
-            int presentChanceNumber = 0;
+            int[] presentChanceNumber = new int[NUMBER_OF_PLAYERS];
             int[] playerPosition = new int[NUMBER_OF_PLAYERS];
             int[] playerNumber = new int[NUMBER_OF_PLAYERS];
             for (int assign = 0; assign < NUMBER_OF_PLAYERS; assign++)
             {
                 playerNumber[assign] = assign + 1;
                 playerPosition[assign] = START_POSITION;
+                presentChanceNumber[assign] = 0;
             }
 
             Random random = new Random();
 
             while (playerPosition[presentPlayer-1] < 100)
             {
-                if (playerNumber[presentPlayer-1] == 1)
-                    presentChanceNumber++;
 
                 numberOnDie = random.Next(1, 7);
                 optionNumber = random.Next(0, 3);
@@ -37,27 +36,30 @@ namespace SnakeAndLadder
                 switch (optionNumber)
                 {
                     case NO_PLAY:
+                        presentChanceNumber[presentPlayer - 1]++;
                         break;
                     case LADDER:
+                        presentChanceNumber[presentPlayer - 1]++;
                         if (playerPosition[presentPlayer-1] + numberOnDie <= 100)
                             playerPosition[presentPlayer-1] += numberOnDie;
                         break;
                     case SNAKE:
+                        presentChanceNumber[presentPlayer - 1]++;
                         playerPosition[presentPlayer-1] -= numberOnDie;
                         if (playerPosition[presentPlayer-1] < START_POSITION)
-                            playerPosition[presentPlayer-1] = 0;
+                            playerPosition[presentPlayer-1] = START_POSITION;
                         break;
 
                 }
-                if (playerPosition[presentPlayer - 1] != 100)
+                if (playerPosition[presentPlayer - 1] != 100 && optionNumber != LADDER)
                     presentPlayer++;
                 if (presentPlayer > NUMBER_OF_PLAYERS)
                     presentPlayer = 1;
 
-                Console.WriteLine("The position of player {0} after die roll number {1} is {2}",presentPlayer, presentChanceNumber, playerPosition[presentPlayer-1]);
+                Console.WriteLine("The position of player {0} after die roll number {1} is {2}",presentPlayer, presentChanceNumber[presentPlayer-1], playerPosition[presentPlayer-1]);
 
             }
-            Console.WriteLine("The Winner is player number {0} and total no of turns is {1}", presentPlayer, presentChanceNumber);
+            Console.WriteLine("The Winner is player number {0} and total no of turns is {1}", presentPlayer, presentChanceNumber[presentPlayer-1]);
 
         }
     }
